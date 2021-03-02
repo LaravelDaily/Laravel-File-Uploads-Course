6ba0da5b913874ff46b19408f361cf92659e737d
ModestasV <info@modestasv.lt>
2021-03-02 12:44:46 +0200

    Chunk upload completed

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,40 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\TemporaryFile;
+use File;
 use Illuminate\Http\Request;
+use Storage;
 
 class UploadController extends Controller
 {
     public function store(Request $request)
     {
         $folder = uniqid() . '-' . now()->timestamp;
-        mkdir(storage_path('app/public/avatars/tmp/' . $folder));
+
+        mkdir(storage_path('app/public/avatars/tmp/' . $folder), 0777, true);
+
+        file_put_contents(storage_path('app/public/avatars/tmp/' . $folder . '/file.part'), '');
+
         return $folder;
-
-        // Old version - if done without chunking
-        if ($request->hasFile('avatar')) {
-            $file = $request->file('avatar');
-            $filename = $file->getClientOriginalName();
-            $folder = uniqid() . '-' . now()->timestamp;
-            $file->storeAs('avatars/tmp/' . $folder, $filename);
-
-            TemporaryFile::create([
-                'folder' => $folder,
-                'filename' => $filename
-            ]);
-
-            return $folder;
-        }
-
-        return '';
     }
 
     public function update(Request $request)
     {
-        // TODO:
-        // - process the chunk and save it somewhere
-        // - somehow catch if it's the last chunk, and then combine
-        // - return success to the filepond
+        $path = storage_path('app/public/avatars/tmp/' . $request->query('patch') . '/file.part');
+
+        File::append($path, $request->getContent());
+
+        // The code below should probably be going into a POST controller method
+        // As the documentation recommends us to actually move the file once it's done.
+        if (filesize($path) == $request->header('Upload-Length')) {
+            $name = $request->header('Upload-Name');
+
+            File::move($path, storage_path("app/public/avatars/{$name}"));
+        }
+
+        return response()->json(['uploaded' => true]);
     }
 
 }
